8a6b6ef0bc81c8ace8bd5ccbd8062727de176834
Sydney Aurelius <sydneyaurelius@yahoo.com>
2025-03-27 14:53:47 -0400

    Onboarding pages; Login, Signup

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,9 @@ import PreviousMeetings from "./pages/PreviousMeetings.jsx";
 import Contacts from "./pages/Contacts.jsx";
 import FAQ from "./pages/FAQ.jsx";
 import MeetingHeatmap from './pages/MeetingHeatmap.jsx';
-import Login from './pages/Login.jsx';
-import Signup from './pages/Signup.jsx';
+import Login from './pages/Onboarding/Login.jsx';
+import Signup from './pages/Onboarding/Signup.jsx';
+import Onboarding from './pages/Onboarding/Onboarding.jsx';
 import { AuthProvider } from "./AuthProvider.jsx";
 
 createRoot(document.getElementById('root')).render(
@@ -27,6 +28,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/meeting-heatmap" element={<MeetingHeatmap /> }/>
           <Route path="/login" element={<Login /> }/>
           <Route path="/signup" element={<Signup /> }/>
+          <Route path="/onboarding" element={<Onboarding /> }/>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
