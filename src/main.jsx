4d81895eb528614a4ffac598d85dc139a25cd592
zeuchie <48141433+zeuchie@users.noreply.github.com>
2025-03-30 18:20:47 -0400

    Created ViewMeeting route, improved Calendar.css, and added mock events to Calendar for testing.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import App from './App.jsx'
 import HomeCalendar from "./pages/Calendar.jsx";
 import CreateMeeting from "./pages/CreateMeeting.jsx";
+import ViewMeeting from "./pages/ViewMeeting.jsx";
 import PreviousMeetings from "./pages/PreviousMeetings.jsx";
 import ContactsList from "./pages/ContactsList.jsx";
 import FAQ from "./pages/FAQ.jsx";
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')).render(
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<HomeCalendar /> }/>
           <Route path="/create-meeting" element={<CreateMeeting /> }/>
+          <Route path="/meeting/:meetingId" element={<ViewMeeting />} />
           <Route path="/previous-meetings" element={<PreviousMeetings /> }/>
           <Route path="/contacts-list" element={<ContactsList /> }/>
           <Route path="/FAQ" element={<FAQ /> }/>
